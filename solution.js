556eb65033562f7f0e02b71bbb55074b31dc35fa
Dave Corbett <davidpcorbett@gmail.com>
2017-11-25 12:25:51 +0000

    Insert

--- solution.js
+++ solution.js
@@ -1,20 +1,19 @@
 var mongo = require('mongodb').MongoClient
 var url = "mongodb://localhost:27017/learnyoumongo"
+var firstName = process.argv[2]
+var lastName = process.argv[3]
+var user = {
+  firstName: firstName
+, lastName: lastName
+}
+
 mongo.connect(url, function(err, db) {
     // db gives access to the database
     if (err) throw err
-    var parrots = db.collection('parrots')
-    parrots.find({
-        age: {
-            $gt: +process.argv[2]
-        }
-    }, {
-        name: 1
-        , age: 1
-        , _id: 0
-    }).toArray(function(err, documents) {
+    var users = db.collection('docs')
+    users.insert(user, (err, data) => {
         if (err) throw err
-        console.log(documents)
+        console.log(JSON.stringify(user))
         db.close()
     })
 })
