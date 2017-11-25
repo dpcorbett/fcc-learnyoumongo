1fe91bf0234db6e00a28cef567d4afaf63bcb7bd
Dave Corbett <davidpcorbett@gmail.com>
2017-11-25 11:55:20 +0000

    Find

---
+++ solution.js
@@ -0,0 +1,16 @@
+var mongo = require('mongodb').MongoClient
+var url = "mongodb://localhost:27017/learnyoumongo"
+mongo.connect(url, function(err, db) {
+    // db gives access to the database
+    if (err) throw err
+    var parrots = db.collection('parrots')
+    parrots.find({
+    age: {
+      $gt: +process.argv[2]
+    }
+}).toArray(function(err, documents) {
+    if (err) throw err
+    console.log(documents)
+    db.close()
+    })
+})
