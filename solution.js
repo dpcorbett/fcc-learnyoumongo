9afa1a0e65d2f284fd7721d56d35de3745438601
Dave Corbett <davidpcorbett@gmail.com>
2017-11-25 13:00:13 +0000

    Count

--- solution.js
+++ solution.js
@@ -1,14 +1,18 @@
 var mongo = require('mongodb').MongoClient
-var url = "mongodb://localhost:27017/"+process.argv[2]
+var url = "mongodb://localhost:27017/learnyoumongo"
+var age = process.argv[2]
 
 mongo.connect(url, function(err, db) {
     // db gives access to the database
     if (err) throw err
-    var collection = db.collection(process.argv[3])
-    collection.remove({
-        _id: process.argv[4]
-    }, (err) => {
+    var collection = db.collection('parrots')
+    collection.count({
+        age: {
+            $gt: +age
+        }
+    }, (err, count) => {
         if (err) throw err
+        console.log(count)
         db.close()
     })
 })
