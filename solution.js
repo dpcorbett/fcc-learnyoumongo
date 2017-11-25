28410b38589ea7e2df3b31134007e916577dd049
Dave Corbett <davidpcorbett@gmail.com>
2017-11-25 12:42:13 +0000

    Update

--- solution.js
+++ solution.js
@@ -1,19 +1,18 @@
 var mongo = require('mongodb').MongoClient
-var url = "mongodb://localhost:27017/learnyoumongo"
-var firstName = process.argv[2]
-var lastName = process.argv[3]
-var user = {
-  firstName: firstName
-, lastName: lastName
-}
+var url = "mongodb://localhost:27017/"+process.argv[2]
 
 mongo.connect(url, function(err, db) {
     // db gives access to the database
     if (err) throw err
-    var users = db.collection('docs')
-    users.insert(user, (err, data) => {
-        if (err) throw err
-        console.log(JSON.stringify(user))
-        db.close()
-    })
+    var users = db.collection('users')
+    users.update({
+        username: "tinatime"
+        }, {
+            $set: {
+                age: 40
+            }
+        }, (err) => {
+            if (err) throw err
+            db.close()
+        })
 })
