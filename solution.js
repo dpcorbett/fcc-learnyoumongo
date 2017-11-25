92d66da9f7cd90df570e1366b8b97b3b8c1cd6a2
Dave Corbett <davidpcorbett@gmail.com>
2017-11-25 12:13:11 +0000

    Find Project

--- solution.js
+++ solution.js
@@ -5,12 +5,16 @@ mongo.connect(url, function(err, db) {
     if (err) throw err
     var parrots = db.collection('parrots')
     parrots.find({
-    age: {
-      $gt: +process.argv[2]
-    }
-}).toArray(function(err, documents) {
-    if (err) throw err
-    console.log(documents)
-    db.close()
+        age: {
+            $gt: +process.argv[2]
+        }
+    }, {
+        name: 1
+        , age: 1
+        , _id: 0
+    }).toArray(function(err, documents) {
+        if (err) throw err
+        console.log(documents)
+        db.close()
     })
 })
